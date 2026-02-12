1cf3b3d4dddd6a1bd47f0815eabd7793a0a97633
Francisco <farenas9134@gmail.com>
2026-02-11 21:34:41 -0600

    basic pipeline to extract features

--- manager.py
+++ manager.py
@@ -2,6 +2,7 @@ from paths import *
 import pandas as pd
 import numpy as np
 import random
+from Model.extract import *
 from sklearn.model_selection import train_test_split
 
 '''
@@ -12,6 +13,14 @@ if __name__ == "__main__":
 
     # Load in dataframe
     twitter_dataset_csv_df = pd.read_csv(TWITTER_HUMAN_DATASET)
+
+    # Checking each column feature, cvs is nasty
+    # print(twitter_dataset_csv_df.columns)
+
+    # Extract features from each tweet and replace tweet column with these new extracted features
+    tweet_column = twitter_dataset_csv_df.loc[:, 'description':'description']
+    extracted_tweet_feature_df = setFeaturesdf(tweet_column)
+
 
     # Split dataset into features and labels
     X = twitter_dataset_csv_df.loc[:, 'Unnamed: 0':'account_age_days']
@@ -26,4 +35,4 @@ if __name__ == "__main__":
     print('Test data shape: ', X_test.shape)
     print('Test labels shape: ', y_test.shape)
 
-    # To be continued. Need to assign values to tweets
+    # To be continued. Need to extract features from tweets
