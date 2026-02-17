9493358bcc841e1b65873d8c4eacb057a3b70eaf
shalimmontes <shalimmontesh@gmail.com>
2026-02-16 23:13:27 -0600

    Added more tweet feature extraction stuff

--- Model/getfeatures.py
+++ Model/getfeatures.py
@@ -1,5 +1,15 @@
 import pandas as pd
 import re
+import spacy 
+
+'''
+    Helper function that tokenizes tweets by white space
+'''
+def splitTweet(tweet):
+
+    split_tweet = tweet.split()
+
+    return split_tweet
 
 '''
     Counts the number of words in a tweet
@@ -17,7 +27,7 @@ def countWords(tweet):
 def countHashtag(tweet):
     num_hashtags = 0
 
-    split_tweet = tweet.split()
+    split_tweet = splitTweet(tweet)
 
     for word in split_tweet:
         if word[0] == "#" and len(word) > 1:
@@ -32,7 +42,7 @@ def countHashtag(tweet):
 def countMentions(tweet):
     num_mentions = 0
 
-    split_tweet = tweet.split()
+    split_tweet = splitTweet(tweet)
 
     for word in split_tweet:
         if word[0] == "@" and len(word) > 1:
@@ -48,7 +58,7 @@ def countRepeats(tweet):
     word_dict = {}
     repeated_words = {}
 
-    split_tweet = tweet.split()
+    split_tweet = splitTweet(tweet)
 
     for word in split_tweet:
         if word not in word_dict:
@@ -61,15 +71,92 @@ def countRepeats(tweet):
     
     return repeated_words
 
+'''
+    Counts the number of questions in a tweet
+'''
+def questionCount(sentences):
+    question_count = 0
+
+    for sentence in sentences:
+        if sentence.strip().endswith("?"):
+            question_count += 1
+
+    return question_count  
+
+'''
+    Counts the number of exclamations in a tweet
+'''
+def exclamationCount(sentences):
+    exclamation_count = 0
+
+    for sentence in sentences:
+        if sentence.strip().endswith("!"):
+            exclamation_count += 1
+
+    return exclamation_count  
+
+'''
+    Returns the number of questions and exclamations in a tweet
+'''
+def getNumQuestionsAndExclamations(tweet):
+    # Splits a tweet into sentences if there is punctuation like ?, !, or .
+    sentences = re.split(r'(?<=[?!.])\s+', tweet)
+
+    question_count = questionCount(sentences)
+    exclamation_count = exclamationCount(sentences)
+
+    return question_count, exclamation_count
+
+''''
+    Counts the number of characters that are not alphanumeric or white space
+'''
+def countSpecialChars(tweet):
+    special_chars = 0
+
+    for char in tweet:
+        if not char.isalnum() and not char.isspace():
+            special_chars += 1
+
+    return special_chars
+
+def countPartsOfSpeech(tweet):
+    nlp = spacy.load("en_core_web_sm")
+
+    doc = nlp(tweet)
+
+    noun_list = []
+    adjective_list = []
+    verb_list = []
+    pronoun_list = []
+    adverb_list = []
+    for token in doc:
+        if token.pos_ == "NOUN":
+            noun_list.append(token)
+        elif token.pos_ == "ADJ":
+            adjective_list.apppend(token)
+        elif token.pos_ == "VERB":
+            verb_list.append(token)
+        elif token.pos_ == "PRON":
+            pronoun_list.append(token)
+        elif token.pos_ == "ADV":
+            adverb_list.append(token)
+        
+    parts_of_speech_dict = {"NOUN": len(noun_list), "ADJECTIVE": len(adjective_list), "ADVERB": len(adverb_list), "VERB": len(verb_list), "PRONOUN": len(pronoun_list)}
+
+    return parts_of_speech_dict
+
 def main():
     # df = pd.read_csv("Datasets/twitter-human-bots-english.csv")
-    sample_tweet = "Loan coach at @mancity & Aspiring DJ loan loan #soccer #music @"
+    sample_tweet = "Loan coach at @mancity & Aspiring DJ #soccer #music @"
+    # sample_tweet = "I'm doing this quickly"
 
     # print(countWords(sample_tweet))
     # print(countHashtag(sample_tweet))
     # print(countMentions(sample_tweet))
-    print(countRepeats(sample_tweet))
-
+    # print(countRepeats(sample_tweet))
+    # print(questionCount(sample_tweet))
+    # print(countSpecialChars(sample_tweet))
+    print(countPartsOfSpeech(sample_tweet))
 
 
 main()
